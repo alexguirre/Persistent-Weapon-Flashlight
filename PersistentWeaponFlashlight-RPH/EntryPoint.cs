4538784f3c9e121c8ccf90e32ff4f5d4aba6f07f
alexguirre <alexguirre.aa@gmail.com>
2020-08-18 16:11:48 +0200

    Added support for v2060
    `CPed_WeaponManagerOffset` changed. Now using patterns to get the offsets that changed so far.

--- PersistentWeaponFlashlight-RPH/EntryPoint.cs
+++ PersistentWeaponFlashlight-RPH/EntryPoint.cs
@@ -9,29 +9,26 @@
 
     internal static unsafe class EntryPoint
     {
-        const string UpdateWeaponComponentFlashlightFunctionPattern = "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 7C 24 ?? 41 54 41 56 41 57 48 83 EC 40 48 8B FA 48 8B D9 48 85 D2 0F 84 ?? ?? ?? ?? 80 7A 28 04 0F 85 ?? ?? ?? ??";
-        const string ToggleWeaponFlashlightFunctionPattern = "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 41 56 41 57 48 83 EC 20 8A 41 49 48 8B FA 48 8B 51 10 44 8A C0 24 FE 4C 8B F9 41 F6 D0 41 80 E0 01 44 0A C0 41 80 C8 02 44 88 41 49 48 8D 0D ?? ?? ?? ??";
-
         delegate void ToggleWeaponFlashlightDelegate(long compFlashlight, IntPtr ped, long unk_0);
 
         /// <summary>
         /// Nops the instruction that turns off the flashlight when not aiming.
         /// </summary>
-        static Nopper nopper1 = new Nopper(Game.FindPattern(UpdateWeaponComponentFlashlightFunctionPattern) + 0x218, 4);
+        static readonly Nopper nopper1 = new Nopper(MemoryFunctions.UpdateWeaponComponentFlashlightFunctionAddr + 0x218, 4);
         /// <summary>
         /// Nops the instruction that calls the ToggleWeaponFlashlight function when the WeaponSpecialTwo control is pressed.
         /// </summary>
-        static Nopper nopper2 = new Nopper(Game.FindPattern(UpdateWeaponComponentFlashlightFunctionPattern) + 0x186, 5);
+        static readonly Nopper nopper2 = new Nopper(MemoryFunctions.UpdateWeaponComponentFlashlightFunctionAddr + 0x186, 5);
         /// <summary>
         /// Nops the instruction that calls the ToggleWeaponFlashlight function when it isn't on and the ped is aiming for components with ToggleWhenAiming set to true in their info, such as COMPONENT_FLASHLIGHT_LIGHT.
         /// </summary>
-        static Nopper nopper3 = new Nopper(Game.FindPattern(UpdateWeaponComponentFlashlightFunctionPattern) + 0x143, 5);
+        static readonly Nopper nopper3 = new Nopper(MemoryFunctions.UpdateWeaponComponentFlashlightFunctionAddr + 0x143, 5);
         /// <summary>
         /// Nops the instruction that calls the ToggleWeaponFlashlight function when it isn't off and the ped isn't aiming for components with ToggleWhenAiming set to true in their info, such as COMPONENT_FLASHLIGHT_LIGHT.
         /// </summary>
-        static Nopper nopper4 = new Nopper(Game.FindPattern(UpdateWeaponComponentFlashlightFunctionPattern) + 0x1FE, 5);
+        static readonly Nopper nopper4 = new Nopper(MemoryFunctions.UpdateWeaponComponentFlashlightFunctionAddr + 0x1FE, 5);
 
-        static ToggleWeaponFlashlightDelegate toggleWeaponFlashlight = Marshal.GetDelegateForFunctionPointer<ToggleWeaponFlashlightDelegate>(Game.FindPattern(ToggleWeaponFlashlightFunctionPattern));
+        static readonly ToggleWeaponFlashlightDelegate toggleWeaponFlashlight = Marshal.GetDelegateForFunctionPointer<ToggleWeaponFlashlightDelegate>(MemoryFunctions.ToggleWeaponFlashlightFunctionAddr);
 
         private static void Main()
         {
--- PersistentWeaponFlashlight-RPH/Memory/MemoryFunctions.cs
+++ PersistentWeaponFlashlight-RPH/Memory/MemoryFunctions.cs
@@ -4,8 +4,14 @@
 
     using Rage;
 
-    internal static class MemoryFunctions
+    internal static unsafe class MemoryFunctions
     {
+        const string UpdateWeaponComponentFlashlightFunctionPattern = "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 7C 24 ?? 41 54 41 56 41 57 48 83 EC 40 48 8B FA 48 8B D9 48 85 D2 0F 84 ?? ?? ?? ?? 80 7A 28 04 0F 85",
+                     ToggleWeaponFlashlightFunctionPattern = "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 41 56 41 57 48 83 EC 20 8A 41 49 48 8B FA 48 8B 51 10 44 8A C0 24 FE 4C 8B F9 41 F6 D0 41 80 E0 01 44 0A C0 41 80 C8 02 44 88 41 49 48 8D 0D";
+
+        public static readonly IntPtr UpdateWeaponComponentFlashlightFunctionAddr = Game.FindPattern(UpdateWeaponComponentFlashlightFunctionPattern),
+                                      ToggleWeaponFlashlightFunctionAddr = Game.FindPattern(ToggleWeaponFlashlightFunctionPattern);
+
         static readonly int CPed_WeaponManagerOffset,
                             CWeaponManager_CurrentWeaponObjectOffset,
                             CObject_WeaponOffset,
@@ -14,31 +20,47 @@
 
         static MemoryFunctions()
         {
-            switch (Game.ProductVersion.Build)
+            IntPtr weaponComponentFlashlightOffsetAddr = Game.FindPattern("48 89 BB ?? ?? ?? ?? EB 16 48 89 BB");
+
+            bool error = false;
+            if (UpdateWeaponComponentFlashlightFunctionAddr == IntPtr.Zero)
             {
-                default:
-                case 1103:
-                    CPed_WeaponManagerOffset = 0x10C8;
-                    CWeaponManager_CurrentWeaponObjectOffset = 0x0078;
-                    CObject_WeaponOffset = 0x0340;
-                    CWeapon_WeaponComponentFlashlightOffset = 0x0148;
-                    CWeaponComponentFlashLight_StateOffset = 0x0049;
-                    break;
-                case 1032:
-                case 1011:
-                    CPed_WeaponManagerOffset = 0x10C8;
-                    CWeaponManager_CurrentWeaponObjectOffset = 0x0078;
-                    CObject_WeaponOffset = 0x0340;
-                    CWeapon_WeaponComponentFlashlightOffset = 0x0140;
-                    CWeaponComponentFlashLight_StateOffset = 0x0049;
-                    break;
+                Game.LogTrivial($"ERROR: could not find {nameof(UpdateWeaponComponentFlashlightFunctionAddr)}");
+                error = true;
             }
+
+            if (ToggleWeaponFlashlightFunctionAddr == IntPtr.Zero)
+            {
+                Game.LogTrivial($"ERROR: could not find {nameof(ToggleWeaponFlashlightFunctionAddr)}");
+                error = true;
+            }
+
+            if (weaponComponentFlashlightOffsetAddr == IntPtr.Zero)
+            {
+                Game.LogTrivial($"ERROR: could not find {nameof(weaponComponentFlashlightOffsetAddr)}");
+                error = true;
+            }
+
+            if (error)
+            {
+                Game.LogTrivial("Unloading...");
+                Game.UnloadActivePlugin();
+                return;
+            }
+
+            CWeaponManager_CurrentWeaponObjectOffset = 0x0078;
+            CObject_WeaponOffset = 0x0340;
+            CWeaponComponentFlashLight_StateOffset = 0x0049;
+
+            CPed_WeaponManagerOffset = *(int*)(ToggleWeaponFlashlightFunctionAddr + 0x68);
+
+            CWeapon_WeaponComponentFlashlightOffset = *(int*)(weaponComponentFlashlightOffsetAddr + 3);
         }
 
-        public static unsafe long GetPedWeaponManager(IntPtr pedPtr) => *(long*)(pedPtr + CPed_WeaponManagerOffset);
-        public static unsafe long GetWeaponManagerCurrentWeaponObject(long weaponMgr) => *(long*)(weaponMgr + CWeaponManager_CurrentWeaponObjectOffset);
-        public static unsafe long GetObjectWeapon(long obj) => *(long*)(obj + CObject_WeaponOffset);
-        public static unsafe long GetWeaponComponentFlashlight(long weapon) => *(long*)(weapon + CWeapon_WeaponComponentFlashlightOffset);
-        public static unsafe bool GetComponentFlashlightIsOn(long component) => (*(byte*)(component + CWeaponComponentFlashLight_StateOffset) & 1) != 0;
+        public static long GetPedWeaponManager(IntPtr pedPtr) => *(long*)(pedPtr + CPed_WeaponManagerOffset);
+        public static long GetWeaponManagerCurrentWeaponObject(long weaponMgr) => *(long*)(weaponMgr + CWeaponManager_CurrentWeaponObjectOffset);
+        public static long GetObjectWeapon(long obj) => *(long*)(obj + CObject_WeaponOffset);
+        public static long GetWeaponComponentFlashlight(long weapon) => *(long*)(weapon + CWeapon_WeaponComponentFlashlightOffset);
+        public static bool GetComponentFlashlightIsOn(long component) => (*(byte*)(component + CWeaponComponentFlashLight_StateOffset) & 1) != 0;
     }
 }
