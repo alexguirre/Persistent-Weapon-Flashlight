739ce186e60dfb67dfd83b319fe297c56df7d790
alexguirre <alexguirre.aa@gmail.com>
2023-02-17 18:00:42 +0100

    Bump version to 1.5

--- Src/Properties/AssemblyInfo.cs
+++ Src/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Persistent Weapon Flashlight")]
-[assembly: AssemblyCopyright("Copyright ©  2017-2020 alexguirre")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2023 alexguirre")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ef0339fc-04b8-4e7a-aadf-5e692fd0081a")]
-[assembly: AssemblyVersion("1.4.0.*")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.*")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
 #if RPH
 [assembly: Rage.Attributes.Plugin("Persistent Weapon Flashlight",
                                   Author = "alexguirre",
